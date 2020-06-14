e064092e240f6b8cd23db92e3e82faf58c4f62f0
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-06-14 15:03:00 +0100

    changed port

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const { promisify } = require("util");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
